0dd456a6fb8fe658cab245c4f39e1ab680a6fb8a
Samuel Bitencourt <samuelb2f@outlook.com>
2026-02-22 17:24:01 -0300

    feat(app): #crud CategoryResource
    #java-spring-expert

---
+++ backend/src/main/java/com/project/dscatalog/entities/Category.java
@@ -0,0 +1,53 @@
+package com.project.dscatalog.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Category implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+
+    public Category() {}
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+        return id.equals(category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
