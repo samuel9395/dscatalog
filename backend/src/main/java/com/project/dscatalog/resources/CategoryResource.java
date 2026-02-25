4bcd95f7359f04c5dc93d5eca1ebd3365d83b870
Samuel Bitencourt <samuelb2f@outlook.com>
2026-02-25 20:51:36 -0300

    study(dscatalog): modulo-1 study
    "POST new Category"
    
    Closes: modulo-1

--- backend/src/main/java/com/project/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/project/dscatalog/resources/CategoryResource.java
@@ -3,12 +3,12 @@ package com.project.dscatalog.resources;
 import com.project.dscatalog.dto.CategoryDTO;
 import com.project.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -30,5 +30,12 @@ public class CategoryResource {
         return ResponseEntity.ok().body(dto);
     }
 
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.status(HttpStatus.CREATED).body(dto);
+    }
 
 }
