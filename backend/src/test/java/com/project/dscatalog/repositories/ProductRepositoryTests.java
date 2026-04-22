bbeac4001838d7adb15857e8302e7b62f4676481
Samuel Bitencourt <samuelb2f@outlook.com>
2026-04-22 10:24:40 -0300

    study(dscatalog): "fixtures no JUnit - BeforeEach - Testando save com id nulo"
    #automated-tests

--- backend/src/test/java/com/project/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/project/dscatalog/repositories/ProductRepositoryTests.java
@@ -1,7 +1,9 @@
 package com.project.dscatalog.repositories;
 
 import com.project.dscatalog.entities.Product;
+import com.project.dscatalog.tests.Factory;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -11,13 +13,32 @@ import java.util.Optional;
 @DataJpaTest
 public class ProductRepositoryTests {
 
+    private long existingId;
+    private long countTotalProducts;
+
     @Autowired
     private ProductRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        existingId = 1L;
+        countTotalProducts = 25L;
+    }
+
+    @Test
+    public void saveShouldIdPersistWithAutoIncrementWhenIdIsNull() {
+        Product product = Factory.createProduct();
+        product.setId(null);
+
+        product = repository.save(product);
+
+        Assertions.assertNotNull(product.getId());
+        Assertions.assertEquals(countTotalProducts + 1, product.getId());
+    }
 
     @Test
     public void deleteShouldDeleteObjectWhenIdExists() {
 
-        long existingId = 1L;
         repository.deleteById(existingId);
 
         Optional <Product> result = repository.findById(existingId);
---
+++ backend/src/test/java/com/project/dscatalog/tests/Factory.java
@@ -0,0 +1,21 @@
+package com.project.dscatalog.tests;
+
+import com.project.dscatalog.dto.ProductDTO;
+import com.project.dscatalog.entities.Category;
+import com.project.dscatalog.entities.Product;
+
+import java.time.Instant;
+
+public class Factory {
+
+    public static Product createProduct() {
+        Product product = new Product(1L, "Phone", "Good Phone", 800.0, "https://img.com/img.png", Instant.parse("2020-10-20T03:00:00Z"));
+        product.getCategories().add(new Category(2L, "Electronics"));
+        return product;
+    }
+
+    public static ProductDTO createProductDTO() {
+        Product product = createProduct();
+        return new ProductDTO(product, product.getCategories());
+    }
+}
