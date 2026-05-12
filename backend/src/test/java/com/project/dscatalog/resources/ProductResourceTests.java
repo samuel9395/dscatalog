e9c2c734b566e585611874b3f61891045dd81427
Samuel Bitencourt <samuelb2f@outlook.com>
2026-05-11 22:18:47 -0300

    subindo arquivo completo

--- backend/src/test/java/com/project/dscatalog/resources/ProductResourceTests.java
+++ backend/src/test/java/com/project/dscatalog/resources/ProductResourceTests.java
@@ -1,4 +1,52 @@
 package com.project.dscatalog.resources;
 
+import com.project.dscatalog.dto.ProductDTO;
+import com.project.dscatalog.services.ProductService;
+import com.project.dscatalog.tests.Factory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+/**
+ * Teste na camada de web
+ */
+@WebMvcTest(ProductResource.class)
 public class ProductResourceTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private ProductService service;
+
+    private ProductDTO productDTO;
+    private PageImpl<ProductDTO> page;
+
+    @BeforeEach
+    void setUp() throws Exception {
+
+        productDTO = Factory.createProductDTO();
+        page = new PageImpl<>(List.of(productDTO));
+        when(service.findAllPaged(any())).thenReturn(page);
+    }
+
+    @Test
+    public void findAllShouldReturnPage() throws Exception {
+        ResultActions result = mockMvc.perform(get("/products").accept(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isOk());
+    }
+
 }
